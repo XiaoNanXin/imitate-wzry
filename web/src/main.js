60ee0152e5d24529ad719a6cab56efbd44cb2705
XiaoNanXin <2446848691@qq.com>
2019-12-01 18:14:59 +0800

    完成了轮播图

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "./style.scss";
+import "./assets/sass/style.scss";
 import router from './router'
 Vue.config.productionTip = false
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="home">
-      首页
-  </div>
+  <swiper :options="swiperOption">
+    <swiper-slide ><img class="w-100" src="../assets/images/a806e41784c93c10cc77d7a146892090.jpeg" alt=""></swiper-slide>
+    <swiper-slide ><img class="w-100" src="../assets/images/a806e41784c93c10cc77d7a146892090.jpeg" alt=""></swiper-slide>
+    <swiper-slide ><img class="w-100" src="../assets/images/a806e41784c93c10cc77d7a146892090.jpeg" alt=""></swiper-slide>
+    <div class="swiper-pagination pagination-home text-right px-3 pb-2"  slot="pagination"></div>
+  </swiper>
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {
+        swiperOption: {
+          // some swiper options/callbacks
+          // 所有的参数同 swiper 官方 api 参数
+          // ...
+          pagination: {
+            el: '.pagination-home'
+          },
+          loop:true,
+        }
+      }
+    },
+  }
 </script>
+
+<style lang="scss">
+@import '../assets/sass/_variables.scss';
+  .pagination-home{
+    
+    .swiper-pagination-bullet{
+      opacity: 1;
+      border-radius: 2px;
+      background: map-get($map: $colors, $key: 'white');
+      &.swiper-pagination-bullet-active{
+        background:map-get($map: $colors, $key: 'info');
+      }
+    }
+  }
+</style>
